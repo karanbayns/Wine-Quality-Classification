a46ea440e3e252a5b5631f79bdb616ac7851f7f5
aidanhew <aidan.hew@outlook.com>
2025-12-07 10:54:48 -0800

    fix argument order in read_csv.py

--- src/read_csv.py
+++ src/read_csv.py
@@ -5,7 +5,7 @@ import pandas as pd
 @click.argument('path_read', type = str)
 @click.argument('path_save', type = str)
 @click.option('--delim', type = str)
-def main(path_read, delim=",", path_save):
+def main(path_read, path_save, delim=","):
     # Read the csv file from path_read, which can be a URL or filepath
     pd.read_csv(path_read, sep=delim).to_csv(path_save, index = False)
         
