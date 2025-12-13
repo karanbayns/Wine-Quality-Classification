32411c95cf813532d1148adbd7fee3be264785e4
karanbayns <karanbains1234@gmail.com>
2025-12-13 15:20:10 -0800

    Change src to src.val_data_handle_error

--- test/test_val_data_handle_error.py
+++ test/test_val_data_handle_error.py
@@ -4,7 +4,7 @@ import pandera as pa
 import pytest
 import json
 import logging
-from src import val_data_handle_error  # Assuming the function is imported
+from src.val_data_handle_error import val_data_handle_error  # Assuming the function is imported
 
 # Configure logging to capture output, similar to the example image
 logging.basicConfig(level=logging.INFO)
